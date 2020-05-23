949fdefc9070eb30619b28c4645ad26c2fcc21de
heriberto.torres <eddietorres622@gmail.com>
2020-05-22 20:06:29 -0400

    initial commit

---
+++ userApp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import re
+
+class userManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+        if not EMAIL_REGEX.match(postData['email']):       
+            errors['email'] = "Invalid email address!"
+        if len(postData['fname']) < 2:
+            errors["fname"] = "First Name should be at least 2 characters"
+        if len(postData['lname']) < 2:
+            errors["lname"] = "Lirst Name should be at least 2 characters"
+        if len(postData['pwd']) < 8:
+            errors["pwd_length"] = "Password Must be 10 character long"
+        if postData['pwd'] != postData['confirm_pwd']:
+            errors["pwd"] = "Password Do Not Match"
+        
+        users = User.objects.all()
+        for user in users:
+            if postData['email'] == user.email:
+                errors['email'] = "email is taken"
+
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = userManager()
---
+++ userApp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import *
+import bcrypt
+
+# registration page
+def register(request):
+    return render(request, 'register.html')
+
+# crate user route
+def createUser(request):
+    print(request.POST)
+    if request.method == "GET":
+        return redirect("../")
+    
+    # validate fields on back-end
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect("../")
+    else:
+        # hash password
+        password = request.POST['pwd']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+        # create user
+        User.objects.create(first_name=request.POST['fname'],last_name=request.POST['lname'],email=request.POST['email'],password=pw_hash)
+        return redirect("/")
+
+# login route
+def login(request):
+    if request.method == "GET":
+        return redirect("/")
+    
+    # find user in database
+    user = User.objects.filter(email=request.POST['email'])
+    # if user exists
+    if user:
+        logged_user = user[0]
+        # verify password
+        if bcrypt.checkpw(request.POST['pwd'].encode(), logged_user.password.encode()):
+            # place user in session
+            request.session['userid'] = logged_user.id
+            request.session['fname'] = logged_user.first_name
+            return redirect("/")
+    
+    # user does not exist
+    messages.error(request,'invalid email/password')
+    return redirect("/")
+
+#logout route
+def logout(request):
+    # if user is logged in, clear user specific session variables
+    if 'userid' in request.session:
+        del request.session['userid']
+        del request.session['fname']
+    return redirect('/')
