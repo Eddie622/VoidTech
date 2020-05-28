a71ecd0735dd399f25263c3789cad5903d34767d
heriberto.torres <eddietorres622@gmail.com>
2020-05-28 01:20:32 -0400

    search by category added

--- productApp/static/js/script.js
+++ productApp/static/js/script.js
@@ -1,20 +1,55 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     // place feather icons
     feather.replace();
 
     // Stop dropdown from closing on click within container
-    $('.dropdown-menu').click(function(e) {
+    $('.dropdown-menu').click(function (e) {
         e.stopPropagation();
     });
 
+    // category functionality (csrf_token needed due to post without form)
+    $('.category').click(function (e) {
+        var value = $(this).text()
+        var csrftoken = Cookies.get('csrftoken');
+
+        // align value to category ID of Category model
+        switch (value) {
+            case 'Desktops':
+              value = 1;
+              break;
+            case 'Laptops':
+              value = 2;
+              break;
+            case 'Tablets':
+               value = 3;
+              break;
+            case 'Phones':
+              value = 4;
+              break;
+            case 'Accessories':
+              value = 5;
+        }
+
+        data = {
+            'csrfmiddlewaretoken' : csrftoken,
+            'category' : value
+        }
+
+        $.post("search/", data, function (serverResponse) {
+            $('#productResults').html(serverResponse);
+            feather.replace(); // re-load icons
+            }
+        )
+    });
+
     // Search functionality
-    $('#Search').keyup(function(e) {
+    $('#Search').keyup(function (e) {
         e.preventDefault()
         var data = $('.searchForm').serialize()
 
-        $.post("search/", data, function(serverResponse){
-                $('#productResults').html(serverResponse);
-                feather.replace(); // re-load icons
+        $.post("search/", data, function (serverResponse) {
+            $('#productResults').html(serverResponse);
+            feather.replace(); // re-load icons
             }
         )
     })
--- productApp/views.py
+++ productApp/views.py
@@ -15,16 +15,37 @@ def index(request):
         'products' : Product.objects.all()
     }
 
+    # delete any category saved (resets search results)
+    if 'category' in request.session:
+        del request.session['category']
+
     return render(request, 'index.html', context)
 
 def search(request):
+    if request.method == 'GET':
+        return redirect('/')
+
+    print(request.POST)
+
+    # default context
     context = {
-        'products' : Product.objects.filter(name__icontains = request.POST['Search'])
+        'products' : Product.objects.all()
     }
 
-    if context['products']:
-        print("found products")
+    # categorize
+    if 'category' in request.POST:
+        request.session['category'] = request.POST['category'] # Set category for future reference
+        context['products'] = Product.objects.filter(category = request.session['category'])
+    # search with category
+    elif 'Search' in request.POST and 'category' in request.session:
+        context['products'] = Product.objects.filter(name__icontains = request.POST['Search'], category = request.session['category'])
+    # search no category
     else:
-        print("no products")
+        context['products'] = Product.objects.filter(name__icontains = request.POST['Search'])
+    
+    # if context['products']:
+    #     print("found products")
+    # else:
+    #     print("no products")
 
     return render(request, 'partials/searchResult.html', context)
